80ea4c8b0f63e69e36aa9e4c7ed576f175335ea6
Anuj Ahuja <anujahuja1999@gmail.com>
2017-04-24 19:13:13 +0530

    Signup done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/new'
+     
+  root 'static#home'
 
-	root 'application#hello'
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+resources :users
+	
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
